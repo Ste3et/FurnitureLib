51b7a4266223ac8a1cc5afb9268f6b7af1a33c66
BlackBeltPanda <juellard@gmail.com>
2016-10-23 12:11:11 -0400

    Refix import

--- src/de/Ste3et_C0st/FurnitureLib/ShematicLoader/ProjectLoader.java
+++ src/de/Ste3et_C0st/FurnitureLib/ShematicLoader/ProjectLoader.java
@@ -15,7 +15,6 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.MaterialData;
