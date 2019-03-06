771f0623689e0d83d09ba663b214ae0aed49e35e
Ste3et_C0st <ste3et@dicecraft.de>
2019-03-06 22:14:13 +0100

    Make Furniture Placeable on Walls

--- src/de/Ste3et_C0st/FurnitureLib/main/Protection/ProtectionManager.java
+++ src/de/Ste3et_C0st/FurnitureLib/main/Protection/ProtectionManager.java
@@ -47,6 +47,7 @@ public class ProtectionManager {
 	private boolean blockStateParser(BlockData blockData) {
 		boolean b = false;
 		if(blockData.getMaterial().name().contains("_FENCE")) return true;
+		if(blockData.getMaterial().name().contains("_WALL")) return true;
 		if(blockData instanceof Slab) {return !((Slab) blockData).getType().name().equalsIgnoreCase("BOTTOM");}
 		if(blockData instanceof Stairs){return !((Stairs) blockData).getHalf().name().equalsIgnoreCase("BOTTOM");}
 		return b;
