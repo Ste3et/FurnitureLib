41b11e59504e42b735e6813ab4e84a61b2956f74
Ste3et_C0st <ste3et@dicecraft.de>
2019-12-20 10:53:49 +0100

    ModelBlockAquaticUpdate Material paser debug for IllegalArgument

--- src/de/Ste3et_C0st/FurnitureLib/ModelLoader/ModelBlockAquaticUpdate.java
+++ src/de/Ste3et_C0st/FurnitureLib/ModelLoader/ModelBlockAquaticUpdate.java
@@ -53,7 +53,14 @@ public class ModelBlockAquaticUpdate extends ModelBlock{
 			}
 		}
 		
-		this.blockData = Bukkit.createBlockData(str);
+		try {
+			this.blockData = Bukkit.createBlockData(str);
+		}catch (IllegalArgumentException e) {
+			System.out.println("Furniture Model File: " + yamlConfiguration.getCurrentPath() + " make Problems with:");
+			System.out.println("Parsing of: " + str);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		this.vector = vector;
 	}
 
