c7802873894c908fe2492bd1c960945c862fb4e5
Ste3et_C0st <ste3et@dicecraft.de>
2020-04-24 20:33:09 +0200

    improve /list speed hopefully

--- src/de/Ste3et_C0st/FurnitureLib/Command/listCommand.java
+++ src/de/Ste3et_C0st/FurnitureLib/Command/listCommand.java
@@ -1,5 +1,6 @@
 package de.Ste3et_C0st.FurnitureLib.Command;
 
+import de.Ste3et_C0st.FurnitureLib.main.FurnitureLib;
 import de.Ste3et_C0st.FurnitureLib.main.FurnitureManager;
 import de.Ste3et_C0st.FurnitureLib.main.ObjectID;
 import de.Ste3et_C0st.FurnitureLib.main.Type.SQLAction;
@@ -7,16 +8,20 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.ComponentBuilder.FormatRetention;
 import net.md_5.bungee.api.chat.HoverEvent;
+
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
+import java.util.function.Predicate;
 
 public class listCommand extends iCommand {
 
@@ -33,72 +38,57 @@ public class listCommand extends iCommand {
 		if (!hasCommandPermission(sender))
 			return;
 		List<ObjectID> objectList = new ArrayList<ObjectID>(FurnitureManager.getInstance().getObjectList());
-		Stream<ObjectID> objectStream = objectList.stream().filter(Objects::nonNull).filter(entry -> SQLAction.REMOVE != entry.getSQLAction());
 		AtomicInteger side = new AtomicInteger(0);
-		boolean filter = false, shouldClose = false;
 		String arguments = String.join(" ", args);
 		String filterTypes = "";
 
+		Predicate<ObjectID> filterPredicate = Objects::nonNull;
+		filterPredicate = filterPredicate.and(entry -> SQLAction.REMOVE != entry.getSQLAction());
+		
 		for (String argument : args) {
-			if (shouldClose)
-				break;
+			if(argument.equalsIgnoreCase("list")) continue;
 			argument = argument.toLowerCase();
 			if (argument.startsWith("plugin:") && !filterTypes.contains("plugin")) {
-				if (!hasCommandPermission(sender, ".plugin")) {
-					shouldClose = true;
-					break;
-				}
+				if (!hasCommandPermission(sender, ".plugin")) return;
 				String plugin = argument.replace("plugin:", "");
-				objectStream = objectStream.filter(entry -> entry.getPlugin().equalsIgnoreCase(plugin));
-				filter = true;
+				filterPredicate = filterPredicate.and(entry -> entry.getPlugin().equalsIgnoreCase(plugin));
 				filterTypes += "§7plugin:§a" + plugin + "§8|";
 			} else if (argument.startsWith("world:") && !filterTypes.contains("world")) {
-				if (!hasCommandPermission(sender, ".world")) {
-					shouldClose = true;
-					break;
-				}
+				if (!hasCommandPermission(sender, ".world")) return;
 				String world = argument.replace("world:", "");
-				objectStream = objectStream.filter(entry -> entry.getWorldName().equalsIgnoreCase(world));
-				filter = true;
+				filterPredicate = filterPredicate.and(entry -> entry.getWorldName().equalsIgnoreCase(world));
 				filterTypes += "§7world:§a" + world + "§8|";
 				continue;
 			} else if (argument.startsWith("player:") && !filterTypes.contains("player")) {
-				if (!hasCommandPermission(sender, ".player")) {
-					shouldClose = true;
-					break;
-				}
+				if (!hasCommandPermission(sender, ".player")) return;
+				
 				OfflinePlayer player = Bukkit.getOfflinePlayer(argument.replace("player:", ""));
 				if (Objects.nonNull(player)) {
-					objectStream = objectStream.filter(entry -> entry.getUUID().equals(player.getUniqueId()));
-					filter = true;
+					sender.sendMessage(player.getName());
+					filterPredicate = filterPredicate.and(entry -> entry.getUUID().equals(player.getUniqueId()));
 					filterTypes += "§7player:§a" + player.getName() + "§8|";
+				}else {
+					filterTypes += "§7player:§c" + player.getName() + "§8|";
 				}
 				continue;
 			} else if (argument.startsWith("distance:") && !filterTypes.contains("distance")) {
-				if (!hasCommandPermission(sender, ".distance")) {
-					shouldClose = true;
-					break;
-				}
+				if (!hasCommandPermission(sender, ".distance")) return;
 				if (Player.class.isInstance(sender)) {
 					Player player = Player.class.cast(sender);
 					AtomicInteger distance = new AtomicInteger(0);
 					try {
 						distance.set(Integer.parseInt(argument.replace("distance:", "")));
 						World world = player.getWorld();
-						objectStream = objectStream.filter(entry -> entry.getWorldName().equalsIgnoreCase(world.getName()))
-								.filter(entry -> entry.getStartLocation().distance(player.getLocation()) < distance
-										.get());
-						filter = true;
+						String worldName = world.getName();
+						Location location = player.getLocation();
+						filterPredicate = filterPredicate.and(entry -> entry.getWorldName().equalsIgnoreCase(worldName) && entry.getStartLocation().distance(location) < distance.get());
 						filterTypes += "§7distance:§a" + distance.get() + "§8|";
 					} catch (Exception e) {
 					}
 				}
 				continue;
 			} else {
-				if (!hasCommandPermission(sender)) {
-					shouldClose = true;
-					break;
-				}
+				if (!hasCommandPermission(sender)) return;
 				try {
 					side.set(Integer.parseInt(argument));
 					arguments = arguments.replace(argument, "");
@@ -106,29 +96,31 @@ public class listCommand extends iCommand {
 				}
 			}
 		}
-		if (shouldClose) {
-			objectStream.close();
-			return;
-		} else if (filter) {
+		
+		List<ComponentBuilder> componentList = new ArrayList<ComponentBuilder>();
+		double maxPages = 0;
+		if (!filterTypes.isEmpty()) {
 			HashMap<String, AtomicInteger> projectCounter = new HashMap<String, AtomicInteger>();
 			FurnitureManager.getInstance().getProjects()
 					.forEach(entry -> projectCounter.put(entry.getName(), new AtomicInteger(0)));
-			objectList.forEach(entry -> {
+			AtomicBoolean items = new AtomicBoolean(false);
+			
+			objectList.stream().filter(filterPredicate).forEach(entry -> {
 				AtomicInteger integer = projectCounter.get(entry.getProject());
 				if (Objects.nonNull(integer)) {
 					integer.incrementAndGet();
+					items.set(true);
 				}
 			});
-			long items = projectCounter.entrySet().stream().parallel().filter(entry -> entry.getValue().get() > 0)
-					.count();
-
-			if (items > 0) {
-				List<ComponentBuilder> componentList = new ArrayList<ComponentBuilder>();
+			if (items.get()) {
 				String filters = filterTypes.substring(0, filterTypes.length() - 1);
 				componentList.add(new ComponentBuilder("§7FilterTypes: [" + filters + "§7]"));
 				projectCounter.entrySet().stream()
 						.sorted((k1, k2) -> Integer.compare(k1.getValue().get(), k2.getValue().get()))
-						.filter(entry -> entry.getValue().get() > 0).forEach(entry -> {
+						.filter(entry -> entry.getValue().get() > 0)
+						.skip(itemsEachSide * side.get())
+						.limit(itemsEachSide)
+						.forEach(entry -> {
 							String name = ChatColor.stripColor(entry.getKey());
 							ComponentBuilder builder = new ComponentBuilder(
 									" §8- §e" + name + " §7Models: §e" + entry.getValue().get());
@@ -144,32 +136,33 @@ public class listCommand extends iCommand {
 							}
 							componentList.add(builder);
 						});
-				new objectToSide(componentList, (Player) sender, side.get(), "/furniture " + arguments, itemsEachSide + 1);
+				double count = projectCounter.values().stream().filter(entry -> entry.get() > 0).count();
+				maxPages = Math.ceil(count / ((double) itemsEachSide));
+			}else {
+				sender.sendMessage("§7There are no furniture models are found.");
+    			sender.sendMessage("§7With applied filters -> {" + StringUtils.removeEnd(filterTypes, "|") + "§7}");
+    			return;
 			}
 		} else {
-			List<ComponentBuilder> componentList = new ArrayList<ComponentBuilder>();
-			FurnitureManager.getInstance().getProjects().stream().sorted((k1, k2) -> ChatColor
-					.stripColor(k1.getDisplayName()).compareTo(ChatColor.stripColor(k2.getDisplayName())))
+			FurnitureManager.getInstance().getProjects().stream().sorted((k1, k2) -> k1.getDisplayName().compareTo(k2.getDisplayName()))
+					.skip(itemsEachSide * side.get())
+					.limit(itemsEachSide)
 					.forEach(entry -> {
-						ComponentBuilder builder = new ComponentBuilder(
-								" §8- §7" + ChatColor.stripColor(entry.getDisplayName()));
+						ComponentBuilder builder = new ComponentBuilder(" §8- §7" + ChatColor.stripColor(entry.getDisplayName()));
+						
 						if (sender.hasPermission("furniture.command.debug")) {
-							ComponentBuilder debugInfos = new ComponentBuilder(
-									"§7Placed Objects: §e" + entry.getObjects().size() + "\n");
-							debugInfos.append("§7SystemID: §e" + entry.getName() + "\n");
-							debugInfos.append("§7Size: §e" + entry.getLength() + " §7|§e " + entry.getHeight()
-									+ " §7|§e " + entry.getWidth() + "\n");
-							debugInfos
-									.append("§7Entities: §e" + entry.getModelschematic().getEntityMap().size() + "\n");
-							debugInfos
-									.append("§7Blockcount: §e" + entry.getModelschematic().getBlockMap().size() + "\n");
-							debugInfos.append("§7Plugin: §e" + entry.getPlugin().getName());
+							ComponentBuilder debugInfos = new ComponentBuilder("§7Placed Objects: §e" + entry.getObjects().size() + "\n");
+								debugInfos.append("§7SystemID: §e" + entry.getName() + "\n");
+								debugInfos.append("§7Size: §e" + entry.getLength() + " §7|§e " + entry.getHeight() + " §7|§e " + entry.getWidth() + "\n");
+								debugInfos.append("§7Entities: §e" + entry.getModelschematic().getEntityMap().size() + "\n");
+								debugInfos.append("§7Blockcount: §e" + entry.getModelschematic().getBlockMap().size() + "\n");
+								debugInfos.append("§7Plugin: §e" + entry.getPlugin().getName());
 							builder.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, debugInfos.create()));
 						}
+						
 						if (sender.hasPermission("furniture.command.give")) {
 							ComponentBuilder give = new ComponentBuilder(" §7[§2give§7]");
-							give.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND,
-											"/furniture give " + entry.getName()));
+							give.event(new ClickEvent(ClickEvent.Action.RUN_COMMAND,"/furniture give " + entry.getName()));
 							builder.append(give.create(), FormatRetention.FORMATTING);
 						}
 						
@@ -203,9 +196,17 @@ public class listCommand extends iCommand {
 						}
 						componentList.add(builder);
 					});
-			if (!componentList.isEmpty()) {
-				new objectToSide(componentList, (Player) sender, side.get(), "/furniture " + arguments, itemsEachSide);
-			}
+			double counts = FurnitureManager.getInstance().getProjects().size();
+			double items = itemsEachSide;
+			maxPages = Math.ceil(counts / items);
+		}
+		
+		if (!componentList.isEmpty()) {
+			new objectToSide(componentList, (Player) sender, side.get(), "/furniture " + arguments, itemsEachSide, (int) maxPages);
+		}else {
+			sender.sendMessage(FurnitureLib.getInstance().getLangManager().getString("message.SideNotFound"));
+			sender.sendMessage(FurnitureLib.getInstance().getLangManager().getString("message.SideNavigation").replaceAll("#MAX#", maxPages + ""));
+            return;
 		}
 	}
 }
--- src/de/Ste3et_C0st/FurnitureLib/Command/objectToSide.java
+++ src/de/Ste3et_C0st/FurnitureLib/Command/objectToSide.java
@@ -88,5 +88,66 @@ public class objectToSide {
         }
         p.spigot().sendMessage(builder.append("§8]").create());
     }
+    
+    public objectToSide(List<ComponentBuilder> objList, Player p, Integer currentPage, String command, int objects, int maxPage) {
+        String a = "";
+        String b = "";
+        if(maxPage == 0) maxPage = 1;
+        if (maxPage < 10) {
+            a += "0" + maxPage;
+        } else {
+            a = maxPage + "";
+        }
+        if (currentPage < 10) {
+            b += "0" + (currentPage + 1);
+        } else {
+            b = (currentPage + 1) + "";
+        }
+
+        if (currentPage > maxPage) {
+            p.sendMessage(FurnitureLib.getInstance().getLangManager().getString("message.SideNotFound"));
+            p.sendMessage(FurnitureLib.getInstance().getLangManager().getString("message.SideNavigation").replaceAll("#MAX#", maxPage + ""));
+            return;
+        }
+
+        p.sendMessage("§7§m+--------------------------------------------+§8[§e" + b + "§8/§a" + a + "§8]");
+
+        for (ComponentBuilder obj : objList) {
+        	p.spigot().sendMessage(((ComponentBuilder) obj).create());
+        }
+
+        String prevCommand = null;
+        String nextCommand = null;
+        String prevColor = "§c";
+        String nextColor = "§a";
+
+        if (currentPage > 0) {
+            prevCommand = command + " " + (currentPage - 1);
+        } else {
+            prevColor = "§7";
+        }
+
+        if (currentPage + 1 < maxPage) {
+            nextCommand = command + " " + (currentPage + 1);
+        } else {
+        	nextColor = "§7";
+        }
+
+        ComponentBuilder builder = new ComponentBuilder("§7§m+--------------------------------------------+§8[§e");
+        if (Objects.nonNull(prevCommand)) {
+            builder.append(prevColor + "«").event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, prevCommand));
+        } else {
+            builder.reset().append("§7«");
+        }
+
+        builder.append("§8/§a");
+
+        if (Objects.nonNull(nextCommand)) {
+            builder.append(nextColor + "»").event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, nextCommand));
+        } else {
+            builder.reset().append("§7»");
+        }
+        p.spigot().sendMessage(builder.append("§8]").create());
+    }
 
 }
