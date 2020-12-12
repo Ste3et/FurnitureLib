17d23841d04081e23928bfec7b8b401113847e18
Ste3et_C0st <ste3et@dicecraft.de>
2020-12-12 13:54:36 +0100

    Try to fix Teleportation problem

--- src/de/Ste3et_C0st/FurnitureLib/Events/internal/onPlayerTeleportEvent.java
+++ src/de/Ste3et_C0st/FurnitureLib/Events/internal/onPlayerTeleportEvent.java
@@ -4,14 +4,17 @@ import de.Ste3et_C0st.FurnitureLib.main.FurnitureLib;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class onPlayerTeleportEvent extends EventLibrary implements Listener {
-    @EventHandler
-    public void onPlayerTeleport(PlayerTeleportEvent event) {
+	
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onPayerTeleport(PlayerTeleportEvent event) {
         final Player player = event.getPlayer();
         if (FurnitureLib.getInstance() == null) return;
-        Bukkit.getScheduler().runTaskLater(FurnitureLib.getInstance(), () -> getFurnitureMgr().updatePlayerView(player), 1);
+        Bukkit.getScheduler().runTaskLaterAsynchronously(FurnitureLib.getInstance(), () -> getFurnitureMgr().updatePlayerView(player), 5);
     }
+    
 }
