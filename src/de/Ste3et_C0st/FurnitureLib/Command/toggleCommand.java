ca90338af56e20760e930a7b505d887acfc2f599
ste3et_c0st <ste3et_c0st@192.168.100.40>
2021-07-06 20:21:52 +0300

    improve toggle command

--- src/de/Ste3et_C0st/FurnitureLib/Command/toggleCommand.java
+++ src/de/Ste3et_C0st/FurnitureLib/Command/toggleCommand.java
@@ -1,6 +1,9 @@
 package de.Ste3et_C0st.FurnitureLib.Command;
 
 import de.Ste3et_C0st.FurnitureLib.main.FurnitureLib;
+
+import java.util.UUID;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -21,19 +24,7 @@ public class toggleCommand extends iCommand {
         if (args.length == 1) {
             if (sender instanceof Player) {
                 if (hasCommandPermission(sender)) {
-                    if (!FurnitureLib.getInstance().getFurnitureManager().getIgnoreList().contains(((Player) sender).getUniqueId())) {
-                        FurnitureLib.getInstance().getFurnitureManager().getIgnoreList().add(((Player) sender).getUniqueId());
-                        FurnitureLib.getInstance().getFurnitureManager().removeFurniture((Player) sender);
-                        sender.sendMessage(getLHandler().getString("message.FurnitureToggleCMDOff"));
-                    } else {
-                    	Player player = (Player) sender;
-                    	int chunkX = player.getLocation().getBlockX() >> 4, chunkZ = player.getLocation().getBlockZ() >> 4;
-                    	FurnitureLib.getInstance().getFurnitureManager().getIgnoreList().remove(player.getUniqueId());
-                        Bukkit.getScheduler().runTaskLaterAsynchronously(FurnitureLib.getInstance(), () -> {
-                        	FurnitureLib.getInstance().getFurnitureManager().updatePlayerView(player, chunkX, chunkZ);
-                        }, 5);
-                        sender.sendMessage(getLHandler().getString("message.FurnitureToggleCMDOn"));
-                    }
+                	this.toggle(sender, Player.class.cast(sender));
                 }
             }
         } else if (args.length == 2) {
@@ -44,31 +35,42 @@ public class toggleCommand extends iCommand {
                     return;
                 }
                 
-                if(player.hasPermission("furniturelib.hidemodels")) {
-            		sender.sendMessage(getLHandler().getString("message.FurnitureToggleCantChange"));
-            		return;
-            	}
-                
-                if (!FurnitureLib.getInstance().getFurnitureManager().getIgnoreList().contains(player.getUniqueId())) {
-                    FurnitureLib.getInstance().getFurnitureManager().getIgnoreList().add(player.getUniqueId());
-                    FurnitureLib.getInstance().getFurnitureManager().removeFurniture(player);
-                    sender.sendMessage(getLHandler().getString("message.FurnitureToggleCMDOff"));
-                    if(!sender.getName().equalsIgnoreCase(args[1])) {
-                    	player.sendMessage(getLHandler().getString("message.FurnitureToggleCMDOff"));
-                    }
-                } else {
-                	int chunkX = player.getLocation().getBlockX() >> 4, chunkZ = player.getLocation().getBlockZ() >> 4;
-                    FurnitureLib.getInstance().getFurnitureManager().getIgnoreList().remove(player.getUniqueId());
-                    FurnitureLib.getInstance().getFurnitureManager().updatePlayerView(player, chunkX, chunkZ);
-                    sender.sendMessage(getLHandler().getString("message.FurnitureToggleCMDOn"));
-                    if(!sender.getName().equalsIgnoreCase(args[1])) {
-                    	player.sendMessage(getLHandler().getString("message.FurnitureToggleCMDOn"));
-                    }
-                }
+                this.toggle(sender, player);
             }
         } else {
             sender.sendMessage(getLHandler().getString("message.WrongArgument"));
             return;
         }
+    }
+   
+    private void toggle(CommandSender sender, Player player) {
+    	if(player.hasPermission("furniturelib.hidemodels") && player.isOp() == false) {
+    		sender.sendMessage(getLHandler().getString("message.FurnitureToggleCantChange"));
+    		return;
+    	}
+    	
+    	UUID uuid = player.getUniqueId();
+    	if(FurnitureLib.getInstance().getFurnitureManager().getIgnoreList().contains(uuid)) {
+    		if(FurnitureLib.getInstance().getFurnitureManager().getIgnoreList().remove(uuid)) {
+    			sendFeedback(sender, player, "message.FurnitureToggleCMDOn");
+    			int chunkX = player.getLocation().getBlockX() >> 4, chunkZ = player.getLocation().getBlockZ() >> 4;
+                FurnitureLib.getInstance().getFurnitureManager().updatePlayerView(player, chunkX, chunkZ);
+    			return;
+    		}
+    	}else if(FurnitureLib.getInstance().getFurnitureManager().getIgnoreList().add(uuid)){
+    		sendFeedback(sender, player, "message.FurnitureToggleCMDOff");
+    		FurnitureLib.getInstance().getFurnitureManager().removeFurniture(player);
+    	}else {
+    		sender.sendMessage("hmm something is wrong");
+    	}
+    }
+    
+    
+    private void sendFeedback(CommandSender sender, Player player, String message) {
+    	if(sender.getName().equalsIgnoreCase(player.getName())) {
+    		player.sendMessage(getLHandler().getString(message));
+    		return;
+    	}
+    	player.sendMessage(getLHandler().getString(message));
     }
 }
