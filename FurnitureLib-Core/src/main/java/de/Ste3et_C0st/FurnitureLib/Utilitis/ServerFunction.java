1080dcf4b0276d566e9f25fd4d0265d1de42fad0
Ste3et_C0st <ste3et@dicecraft.de>
2024-01-02 10:22:46 +0100

    add setDisplayName & setLore to Server function for direct ItemMeta modification

--- FurnitureLib-Core/src/main/java/de/Ste3et_C0st/FurnitureLib/Utilitis/ServerFunction.java
+++ FurnitureLib-Core/src/main/java/de/Ste3et_C0st/FurnitureLib/Utilitis/ServerFunction.java
@@ -3,11 +3,16 @@ package de.Ste3et_C0st.FurnitureLib.Utilitis;
 import java.util.List;
 
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import net.md_5.bungee.api.chat.BaseComponent;
 
 public interface ServerFunction {
 	public ItemStack displayName(ItemStack stack, BaseComponent[] baseComponent);
 	public ItemStack lore(ItemStack stack, List<BaseComponent[]> component);
+	
+	public ItemMeta setDisplayName(ItemMeta meta, BaseComponent[] baseComponent);
+	public ItemMeta setLore(ItemMeta meta, List<BaseComponent[]> component);
+	
 	public void onEnable();
 }
