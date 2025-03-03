409ad6fe815be57dd6946f0ade1153632bd3f3fb
Matthias Ihring <Matthias Ihring@GamingPC2025.speedport.ip>
2025-03-03 17:37:44 +0100

    Try to fix version Error

--- FurnitureLib-Core/src/main/java/de/Ste3et_C0st/FurnitureLib/Listener/onCrafting.java
+++ FurnitureLib-Core/src/main/java/de/Ste3et_C0st/FurnitureLib/Listener/onCrafting.java
@@ -3,9 +3,13 @@ package de.Ste3et_C0st.FurnitureLib.Listener;
 import de.Ste3et_C0st.FurnitureLib.Crafting.Project;
 import de.Ste3et_C0st.FurnitureLib.main.FurnitureLib;
 import de.Ste3et_C0st.FurnitureLib.main.FurnitureManager;
+
+import java.lang.reflect.Method;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -14,15 +18,16 @@ public class onCrafting implements Listener {
     private FurnitureManager manager = FurnitureManager.getInstance();
 
     @EventHandler
-    private void onCraftingEvent(PrepareItemCraftEvent e) {
+    private void onCraftingEvent(PrepareItemCraftEvent event) {
         if (manager.getProjects().isEmpty()) {
             return;
         }
-        Player p = (Player) e.getView().getPlayer();
-        if (p.isOp()) return;
-        if (e.getInventory() == null) return;
-        if (e.getInventory().getResult() == null) return;
-        ItemStack is = e.getInventory().getResult().clone();
+        final Player player = FurnitureLib.isVersionOrAbove("1.21") ? (Player) event.getView().getPlayer() : getPlayerFromView(event);
+        if (player == null) return;
+        if (player.isOp()) return;
+        if (event.getInventory() == null) return;
+        if (event.getInventory().getResult() == null) return;
+        ItemStack is = event.getInventory().getResult().clone();
         is.setAmount(1);
         for (Project pro : manager.getProjects()) {
             if (pro == null) continue;
@@ -30,11 +35,22 @@ public class onCrafting implements Listener {
             if (pro.getCraftingFile().getRecipe() == null) continue;
             if (pro.getCraftingFile().getRecipe().getResult() == null) continue;
             if (is.equals(pro.getCraftingFile().getRecipe().getResult())) {
-                if (!hasPermissions(p, pro.getSystemID())) {
-                    e.getInventory().setResult(null);
+                if (!hasPermissions(player, pro.getSystemID())) {
+                	event.getInventory().setResult(null);
                 }
             }
         }
+    }
+    
+    private Player getPlayerFromView(InventoryEvent event) {
+    	try {
+    		Object inventoryView = event.getView();
+    		Method getPlayerMethod = inventoryView.getClass().getMethod("getPlayer");
+    		getPlayerMethod.setAccessible(true);
+    		return (Player) getPlayerMethod.invoke(inventoryView);
+    	}catch (Exception e) {
+    		return null;
+		}
     }
 
     private boolean hasPermissions(Player p, String name) {
